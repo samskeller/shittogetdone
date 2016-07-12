eecda491d4b1db5c942815c82f5e21e49785c7c8
Sam Keller <samskeller@gmail.com>
2016-07-11 22:55:43 -0400

    Adding a basic checkbox form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,23 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField, widgets
 from wtforms.validators import Required
+
+
+class MultipleCheckboxField(SelectField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class ShitToGetDoneForm(Form):
+    to_do = MultipleCheckboxField('Shit')
+
+    def __init__(self, shit, *args, **kwargs):
+        shit_choices = [(x, x) for x in shit]
+        self.to_do.kwargs['choices'] = shit_choices
+        super(ShitToGetDoneForm, self).__init__(*args, **kwargs)
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,15 @@
 from flask import render_template, session, redirect, url_for
 from . import main
-from .forms import NameForm
+from .forms import ShitToGetDoneForm
 from .. import db
 from ..models import User
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    to_get_done = session.get('to_get_done', ['Laundry', 'Dishes'])
+    form = ShitToGetDoneForm(to_get_done)
     if form.validate_on_submit():
         return redirect(url_for('.index'))
+    session['to_get_done'] = ['Wallpaper', 'Cat Litter']
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False))
